4d32c4496919794f1bdace82c2b01199ad87ba02
Pharakit <mr.job4399@gmail.com>
2023-05-31 13:01:38 +0700

    aa

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function index() {
 
       <div className="text-center">
         <Image
-          src="/A.png"
+          src="/catll.png"
           className="rounded"
           alt="test"
           width={250}
