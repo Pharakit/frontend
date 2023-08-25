d4a7f8196df4a44d0cc7a7319dc07b12844a3ee2
Pharakit <mr.job4399@gmail.com>
2023-08-25 13:42:13 +0700

    aa

--- next.config.js
+++ next.config.js
@@ -4,3 +4,17 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.example.com',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
+  },
+}
